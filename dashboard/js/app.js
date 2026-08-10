0f0cf6445a90cd393743b7bc1724750027c810e3
Mecsocse <49002983+Mecsocse@users.noreply.github.com>
2026-08-10 16:35:08 +0200

    Add files via upload

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -334,23 +334,11 @@ class DashboardApp {
             this.quickYearSelect.addEventListener('change', (e) => {
                 if (e.target.value) activateYearMode(e.target.value);
             });
-            this.quickYearSelect.addEventListener('click', (e) => {
-                if (this.currentPeriod !== 'year' || !this.quickYearSelect.value) {
-                    const val = this.quickYearSelect.value || this.selectedYear || '2026';
-                    activateYearMode(val);
-                }
-            });
         }
 
         if (this.quickMonthSelect) {
             this.quickMonthSelect.addEventListener('change', (e) => {
                 if (e.target.value) activateMonthMode(e.target.value);
-            });
-            this.quickMonthSelect.addEventListener('click', (e) => {
-                if (this.currentPeriod !== 'month' || !this.quickMonthSelect.value) {
-                    const val = this.quickMonthSelect.value || this.selectedMonth || '2026-08';
-                    activateMonthMode(val);
-                }
             });
         }
     }
